2224ad9e291970dd4c2c3a96a16c379646668ac2
ingrid.draagen@gmail.com <ingrid.draagen@gmail.com>
2018-09-20 13:17:29 +0200

    adding stuff to my branch, hopefully

--- drainbow/src/App.js
+++ drainbow/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+      <div className="ImgWrap">
+      <img className="Image" src={"../img/1-1.jpg"} alt="happy1"/>
+        <div className="grid-container">
+        <div className="grid-menu">
+          <img className="menu" src={"../img/menu.svg"} alt="menu"/>
+        </div>
+        <div className="grid-sound">
+          <img className="sound" src={"../img/soundOn.svg"} alt="soundOn"/></div>
+        <div className="grid-text"><p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p></div>
+        <div className="grid-slider">
+        <svg className="sliderImage">
+          <circle className="1"  r="7" fill="#D3BDB0" cy="120" />
+        </svg>
+        </div>
+
+        </div>
+
+
+
+
+        </div>
+        </div>
+
     );
   }
 }
