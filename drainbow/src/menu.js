7782cb52d5e992e663779cdd4b6979f6b5e76230
daniel stupar <danielstupar95@gmail.com>
2018-09-21 17:43:17 +0200

    la til dropdown menu

--- drainbow/src/menu.js
+++ drainbow/src/menu.js
@@ -3,32 +3,27 @@ import React, {Component} from 'react';
 class Menu extends Component {
     render() {
         return (
-            <div>
+            <div className="">
 
-                <form>Poems
-                    <input type="radio" id="radiohappypoem" name="poemradio" onClick={() =>this.props.checkRadio("happypoem")} defaultChecked/>
-                    <label>happy</label>
-                    <input type="radio" id="radiosadpoem"  name="poemradio" onClick={() =>this.props.checkRadio("sadpoem")}/>
-                    <label>sad</label>
-                    <input type="radio" id="radioangrypoem"  name="poemradio" onClick={() =>this.props.checkRadio("angrypoem")} />
-                    <label>angry</label>
-                </form>
-                <form>Picture
-                    <input type="radio" id="radiohappyimg" name="imgradio" onClick={() =>this.props.checkRadio("happyimg")}defaultChecked/>
-                    <label>happy</label>
-                    <input type="radio" id="radiosadimg"  name="imgradio" onClick={() =>this.props.checkRadio("sadimg")}/>
-                    <label>sad</label>
-                    <input type="radio" id="radioangryimg"  name="imgradio" onClick={() =>this.props.checkRadio("angryimg")}/>
-                    <label>angry</label>
-                </form>
-                <form>Sound
-                    <input type="radio"  name="soundradio" onClick={() =>this.props.checkRadio("happysound")} defaultChecked/>
-                    <label>happy</label>
-                    <input type="radio"   name="soundradio" onClick={() =>this.props.checkRadio("sadsound")}/>
-                    <label>sad</label>
-                    <input type="radio"   name="soundradio" onClick={() =>this.props.checkRadio("angrysound")} />
-                    <label>angry</label>
-                </form>
+                <select id="Categoryselect" onChange={()=>this.props.checkRadio(document.getElementById("Categoryselect").value)}>
+                    <optgroup label="Poems">
+                        <option value="happypoem">happy</option>
+                        <option value="sadpoem">sad</option>
+                        <option value="angrypoem">angry</option>
+                    </optgroup>
+                    <optgroup label="Pictures">
+                            <option value="happyimg">happy</option>
+                            <option value="sadimg">sad</option>
+                            <option value="angryimg">angry</option>
+                    </optgroup>
+                    <optgroup label="Sound">
+                        <option value="happysound">happy</option>
+                        <option value="sadsound">sad</option>
+                        <option value="angrysound">angry</option>
+                    </optgroup>
+                </select>
+
+
             </div>
         );
     }
